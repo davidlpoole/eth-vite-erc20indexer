b6a81375c3eca7bc630a2f567ee959ea9b522360
David Poole <14233215+davidlpoole@users.noreply.github.com>
2023-08-07 14:40:53 +0100

    Add comments

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,12 @@ import {
     Button,
     Center,
     Flex,
-    Heading,
     Image,
     Input,
     SimpleGrid,
-    Text,
-    Spinner, Card, CardBody, Divider, Stack, CircularProgress,
+    Card,
+    Stack,
+    CircularProgress,
 } from '@chakra-ui/react';
 import {Alchemy, Network, Utils} from 'alchemy-sdk';
 import {useState} from 'react';
@@ -22,38 +22,44 @@ function App() {
     const [tokenDataObjects, setTokenDataObjects] = useState([]);
     const [status, setStatus] = useState('');
 
+
+    // Connect to browser wallet (e.g. MetaMask) and get the active wallet address
     async function getWalletAddress() {
         const accounts = await provider.send('eth_requestAccounts', []);
         setUserAddress(accounts[0]);
     }
 
+
+    // Query the Alchemy Token API to get a list of tokens and balances for the user address
     async function getTokenBalance() {
-        if (userAddress === '') {
+        if (userAddress === '') {   // text input is blank
             setStatus('');
             return
-        } else if (!ethers.utils.isAddress(userAddress)) {
+        } else if (!ethers.utils.isAddress(userAddress)) {  // check textbox input is a valid hex address
             setStatus('invalid')
             return
         }
 
-        setStatus('fetching');
+        setStatus('fetching');  // if input is valid then start querying
 
+        // get the private Alchemy API key and set the network from .env file
         const config = {
             apiKey: import.meta.env.VITE_ALCHEMY_API_KEY,
             network: Network.ETH_MAINNET,
         };
 
+        // connect to the API and get a list of token names and balances
         const alchemy = new Alchemy(config);
         const data = await alchemy.core.getTokenBalances(userAddress);
+
+        // filter the results to only tokens with balance > 0
         const filtered = data.tokenBalances.filter((item) => {
             return item.tokenBalance > 0
         })
-        // const sorted = filtered.sort((a,b)=> a.tokenBalance-b.tokenBalance);
-        // console.log(sorted);
         setResults(filtered);
 
+        // query the API for each token metadata, return promises, then await the promises all at once
         const tokenDataPromises = [];
-
         if (filtered.length > 0) {
             for (let i = 0; i < filtered.length; i++) {
                 const tokenData = alchemy.core.getTokenMetadata(
@@ -61,13 +67,12 @@ function App() {
                 );
                 tokenDataPromises.push(tokenData);
             }
-
-            setTokenDataObjects(await Promise.all(tokenDataPromises));
-            setStatus('loaded');
+            setTokenDataObjects(await Promise.all(tokenDataPromises));  // get the actual metadata
+            setStatus('loaded'); // data has been updated
             return;
         }
 
-        setStatus('notfound')
+        setStatus('notfound')  // no token balances > 0 returned
 
     }
 
@@ -97,18 +102,18 @@ function App() {
                     placeholder="Enter an address or get address from browser wallet"
                 />
 
-                {(userAddress !== '') ? (
-                    <Button fontSize={20} onClick={getTokenBalance} mt={36}>
-                        Check ERC-20 Token Balances
-                    </Button>
-                ) : (
+                {(userAddress === '') ? (   // if text input is blank, show button to get browser wallet addr
                     <Button fontSize={20} onClick={getWalletAddress} mt={36}>
                         Get browser wallet address
+                    </Button>
+                ) : (  // else let user initiate the API query to get balances
+                    <Button fontSize={20} onClick={getTokenBalance} mt={36}>
+                        Check ERC-20 Token Balances
                     </Button>
                 )
                 }
 
-                {(status === 'loaded') ? (
+                {(status === 'loaded') ? (  // token data has been loaded, so display in a grid
                     <div>
                         <SimpleGrid w={'90vw'} mt={36} columns={4} spacing={30}>
                             {results.map((e, i) => {
@@ -132,6 +137,8 @@ function App() {
                     </div>
 
                 ) : null }
+
+                {/* show loading indicator or error message, based on status */}
                 {(status === 'fetching') ? <CircularProgress isIndeterminate mt={36}/> : null}
                 {(status === 'invalid') ? <Box color="red" mt={36}>Invalid address</Box> : null }
                 {(status === 'notfound') ? <Box color="red" mt={36}>No ERC-20 tokens found</Box> : null }
